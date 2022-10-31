5e9e7f3ec39d4de0ea1a44fc2c49c36bdcf825e2
Ndifreke Friday <ndiecodes@gmail.com>
2022-10-31 04:18:01 +0100

    can render if statements - @if & @endif

--- __tests__/momo.test.ts
+++ __tests__/momo.test.ts
@@ -3,3 +3,36 @@ import { render } from '../src/momo';
 test('should render template', () => {
   expect(render('<p>Hello {{name}}</p>', { name: 'Ndie' })).toBe('<p>Hello Ndie</p>');
 });
+
+describe('Test if statement', () => {
+
+  test('test truthy if statement', () => {
+    expect(
+      render(
+        `@if(user.age > 19)
+    <p>{{user.age}} is old enough</p>
+    @endif`,
+        {
+          user: {
+            age: 20,
+          },
+        },
+      ),
+    ).toMatch('<p>20 is old enough</p>');
+  });
+
+  test('test falsy if statement', () => {
+    expect(
+      render(
+        `@if(user.age > 19)
+    <p>{{user.age}} is old enough</p>
+    @endif`,
+        {
+          user: {
+            age: 10,
+          },
+        },
+      ),
+    ).not.toMatch('<p>20 is old enough</p>');
+  });
+});
--- src/momo.ts
+++ src/momo.ts
@@ -3,48 +3,95 @@ interface LooseObject {
 }
 
 class Template {
-  private tokenRegex = '({{|}})';
-  private mode: string | null = null;
+  private tokenRegex = `({{|}}|\@if|\@endif)`;
+  private TOKEN_TYPE: Symbol | null = null;
+  private RENDER: Symbol = Symbol();
+  private IF: Symbol = Symbol();
+  private ENDIF: Symbol = Symbol();
+  private FOR: Symbol = Symbol();
   private source = '';
   public constructor(private template: string) {}
-  public tokenize(): Array<string> {
+  private tokenize(): Array<string> {
     let temp: string = this.template;
     const tokens = [];
     const regex = new RegExp(this.tokenRegex);
     let result = regex.exec(temp);
-
     while (result) {
       let searchIndex = result.index;
+      let token = result[0]
       if (searchIndex !== 0) {
         tokens.push(temp.substring(0, searchIndex));
         temp = temp.slice(searchIndex);
       }
-      tokens.push(result[0]);
-      temp = temp.slice(result[0].length);
+      tokens.push(token);
+      temp = temp.slice(token.length);
+      switch (token) {
+        case "@if":
+          const nextTokenAfterIf = temp.substring(0, this.nextTokenIndex(temp))
+          tokens.push(nextTokenAfterIf)
+          temp = temp.slice(nextTokenAfterIf.length);
+          break;
+        default:
+          break;
+      }
       result = regex.exec(temp);
     }
+
+    /**If there's nothing to tokenize, then it's one giant array element */
     if (temp) {
       tokens.push(temp);
     }
     return tokens;
   }
 
-  public parseToken(token: string) {
+  private nextTokenIndex(template:string) {
+    for(let i = 0; i < template.length; i++) {
+      if(template[i] === ")") {
+        return i + 1
+      }
+    }
+
+    throw new Error("Closing bracket for @ statement not found")
+  }
+
+  public parseToken(token: string, currentIndex: number, totalTokens: number) {
     switch (token) {
       case '{{':
-        this.mode = 'eval';
+        this.TOKEN_TYPE = this.RENDER;
         break;
       case '}}':
-        this.mode = null;
+        this.TOKEN_TYPE = null;
+        break;
+      case '@if':
+        this.TOKEN_TYPE = this.IF;
+        break;
+      case '@endif':
+        if(currentIndex === totalTokens - 1) {
+          this.source += `}`;
+        }
+        this.TOKEN_TYPE = this.ENDIF;
         break;
       default:
-        if (this.mode) {
-          this.source += `; _append(${token})`;
+        if (this.TOKEN_TYPE) {
+          switch (this.TOKEN_TYPE) {
+            case this.RENDER:
+              this.source += ` _append(${token});`;
+              break;
+            case this.IF:
+              this.source += `\nif ${token} {\n`;
+              this.TOKEN_TYPE = null
+              break;
+            case this.ENDIF:
+              this.source += `}`;
+              this.TOKEN_TYPE = null
+              break;
+          }
         } else {
-          this.source += `; _append("${token}")`;
+          this.source += "_append(`" + token+ "`);";
         }
         break;
     }
+
   }
 
   public compile(data: LooseObject) {
@@ -54,17 +101,18 @@ class Template {
     let append = '';
     const tokens = this.tokenize();
 
-    tokens.forEach((token) => {
-      self.parseToken(token);
+    const totalTokens = tokens.length
+
+    tokens.forEach((token, index, array) => {
+      self.parseToken(token, index, totalTokens);
     });
 
     if (this.source) {
-      prepend += `let _output = ''
-        function _append(str){return _output += str}`;
+      prepend += `let _output = '';
+        function _append(str){if(str) return _output += str}`;
 
-      append += `; return _output;`;
+      append += `return _output;`;
       code = prepend + this.source + append;
-
       return Function(`{${Object.keys(data).join(',')}}`, code);
     }
   }
@@ -73,10 +121,5 @@ class Template {
 export function render(template: string, data: LooseObject = {}) {
   const tmpl = new Template(template);
   const fn = tmpl.compile(data);
-
-  if (fn) {
-    console.log(fn(data));
-  }
-
-  return fn ? fn(data) : "";
+  return fn ? fn(data) : '';
 }
